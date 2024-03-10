da454de76c918d5f41d4f09fc856a7e50e5cd0ba
XuKaFy <36410093+XuKaFy@users.noreply.github.com>
2024-03-10 21:50:22 +0800

    加入对于 Lexer 的测试

---
+++ test/src/read_file.h
@@ -0,0 +1,15 @@
+#include "def.h"
+#include <fstream>
+
+pCode read_file(Symbol file_suffix)
+{
+    String file = String("../../code/") + file_suffix;
+    std::ifstream in;
+    in.open(file, std::fstream::in);
+    EXPECT_TRUE(in.is_open());
+
+    String code((std::istreambuf_iterator<char>(in)),  
+                 std::istreambuf_iterator<char>());
+    pCode p_code = make_code(code, file);
+    return p_code;
+}
--- test/src/test_staisp_lexer.cpp
+++ test/src/test_staisp_lexer.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include "staisp_lexer.h"
+#include "read_file.h"
+
+#include <fstream>
 
 TEST(test_staisp_lexer, basic_lexer) {
     Staisp::Lexer lexer;
@@ -13,6 +16,20 @@ TEST(test_staisp_lexer, basic_lexer) {
     code = make_code("TOKEN1 TOKEN2 TOKEN3 1234567abcd", "test1");
     try {
         ans = lexer.lexer(code);
+        EXPECT_TRUE(false);
+    } catch (Exception e) {
+        EXPECT_EQ(e.id, 1);
+        EXPECT_TRUE(strcmp(e.object, "Lexer") == 0);
+    }
+}
+
+TEST(test_staisp_lexer, lexer_error_1) {
+    Staisp::Lexer lexer;
+
+    pCode p_code = read_file("error_lexer/1.sta");
+
+    try {
+        lexer.lexer(p_code);
         EXPECT_TRUE(false);
     } catch (Exception e) {
         EXPECT_EQ(e.id, 1);
