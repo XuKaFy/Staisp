f9853caae8f43c846d68cfdfff3925f26795100f
XuKaFy <36410093+XuKaFy@users.noreply.github.com>
2024-03-10 21:57:52 +0800

    加入了对于两个 common 类的测试

---
+++ test/src/test_common.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "common_token.h"
+#include "common_node.h"
+
+TEST(test_common_token, basic_token) {
+    String s = "hello world";
+    pCode pcode = make_code("hello world", "file1");
+    Token t = Token(pcode, pcode->p_code->begin(), pcode->p_code->end(), 1);
+    try {
+        t.throw_error(10, "test", "null");
+        EXPECT_TRUE(false);
+    } catch (Exception e) {
+        EXPECT_TRUE(strcmp(e.object, "test") == 0);
+        EXPECT_TRUE(strcmp(e.message, "null") == 0);
+        EXPECT_EQ(e.id, 10);
+    }
+}
+
+TEST(test_common_node, basic_node) {
+    String s = "hello world";
+    pCode pcode = make_code("hello world", "file1");
+    Node n = Node(pToken(new Token(pcode, pcode->p_code->begin(), pcode->p_code->end(), 1)), NODE_ASSIGN);
+    EXPECT_EQ(n.type, NODE_ASSIGN);
+    try {
+        n.throw_error(10, "test", "null");
+        EXPECT_TRUE(false);
+    } catch (Exception e) {
+        EXPECT_TRUE(strcmp(e.object, "test") == 0);
+        EXPECT_TRUE(strcmp(e.message, "null") == 0);
+        EXPECT_EQ(e.id, 10);
+    }
+}
--- test/src/test_common_token.cpp
+++
@@ -1,5 +0,0 @@
-#include "gtest/gtest.h"
-#include "common_token.h"
-
-TEST(test_common_token, basic_token) {
-}
