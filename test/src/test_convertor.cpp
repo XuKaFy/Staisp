c8dd632e7784fa2d3c641ac253322b15fa531168
XuKaFy <36410093+XuKaFy@users.noreply.github.com>
2024-03-11 21:59:00 +0800

    加入对于 Convertor 的测试

---
+++ test/src/test_convertor.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "staisp_parser.h"
+#include "convert_ast_to_ir.h"
+#include "read_file.h"
+
+#define TEST_A_FILE(x) \
+TEST(test_convertor, convertor_error_##x) { \
+    pCode p_code = read_test_file("error_convertor/" #x ".sta"); \
+    try { \
+        AstProg root = Staisp::Parser().parse(p_code); \
+        Ir::pModule mod = AstToIr::Convertor().generate(root); \
+        EXPECT_TRUE(false); \
+    } catch (Exception e) { \
+        EXPECT_EQ(e.id, x); \
+        EXPECT_STREQ(e.object, "Convertor"); \
+    } \
+}
+
+TEST_A_FILE(3)
+TEST_A_FILE(5)
+TEST_A_FILE(8)
+TEST_A_FILE(9)
+TEST_A_FILE(10)
+TEST_A_FILE(11)
