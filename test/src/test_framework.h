a30ad6cec123535485fb05e0ca0b36f94ddee4e1
Yaossg <yaossg@outlook.com>
2024-05-21 01:03:51 +0800

    parallel test sysy

--- test/src/test_framework.h
+++ test/src/test_framework.h
@@ -11,7 +11,7 @@
 String read(String file) {
     std::ifstream in;
     in.open(file, std::fstream::in);
-    EXPECT_TRUE(in.is_open());
+    EXPECT_TRUE(in.is_open()) << file;
 
     String code((std::istreambuf_iterator<char>(in)),
                 std::istreambuf_iterator<char>());
@@ -46,49 +46,63 @@ std::string normalizeLineEndings(const std::string &str) {
     return result;
 }
 
-void cmp(String ll) {
+void cmp(String id) {
     prepare();
-    system((String("llvm-as ") + ll + " -o out.bc").c_str());
-    system("llvm-link out.bc sylib.bc -o final.bc");
-    system("lli final.bc < 1.std.in > 1.out ; echo $? > 1.out.ret");
-    String actual = read("1.out");
-    String tret = read("1.out.ret");
-    String expected = read("1.std.out");
+    system(("llvm-as " + id + ".sy.ll -o " + id + "_out.bc").c_str());
+    system(("llvm-link " + id + "_out.bc sylib.bc -o " + id + "_final.bc").c_str());
+    system(("lli " + id + "_final.bc < " + id + ".std.in > " + id + ".out ; echo $? > " + id + ".out.ret").c_str());
+    String actual = read(id + ".out");
+    String tret = read(id + ".out.ret");
+    String expected = read(id + ".std.out");
     if (!actual.empty() && actual.back() != '\n')
         actual += "\n";
     actual += tret;
     EXPECT_EQ(normalizeLineEndings(actual), normalizeLineEndings(expected));
 }
 
-void run_sysy(String file) {
-    // in ./build/test
-    int res = system(
-        (String("../frontend/SysYFrontend ../../") + file + ".sy").c_str());
-    if (res) {
-        EXPECT_EQ(0, 1);
-        return;
+String get_id(String path) {
+    for (char& ch : path) {
+        if (ch == '/') ch = '_';
     }
-    system((String("cp ../../") + file + ".out 1.std.out").c_str());
-    system((String("cp ../../") + file + ".in 1.std.in").c_str());
-    system((String("cp ../../") + file + ".sy.opt.ll 1.sy.opt.ll").c_str());
-    system((String("cp ../../") + file + ".sy.ll 1.sy.ll").c_str());
-    printf("running %s.sy\n", file.c_str());
-    cmp("1.sy.ll");
-    // cmp("1.sy.opt.ll");
+    return path;
 }
 
-void run_staisp(String file) {
-    int res =
-        system((String("../staisp_frontend ../..") + file + ".sta").c_str());
-    if (res) {
-        EXPECT_EQ(0, 1);
-        return;
+void mov(String path, String id, Symbol suffix1, Symbol suffix2) {
+    std::string command;
+    command += "cp ../../";
+    command += path;
+    command += suffix1;
+    command += " ";
+    command += id;
+    command += suffix2;
+    if (system(command.c_str())) {
+        system(("touch " + id + suffix2).c_str());
     }
-    system((String("cp ../../") + file + ".out 1.std.out").c_str());
-    system((String("cp ../../") + file + ".in 1.std.in").c_str());
-    system((String("cp ../../") + file + ".sta.opt.ll 1.sta.opt.ll").c_str());
-    system((String("cp ../../") + file + ".sta.ll 1.sta.ll").c_str());
-    printf("running %s.sta\n", file.c_str());
-    cmp("1.sta.ll");
-    cmp("1.sta.opt.ll");
 }
+
+void run_sysy(String path) {
+    // in ./build/test
+    int res = system(("../frontend/SysYFrontend ../../"+ path + ".sy").c_str());
+    ASSERT_EQ(res, 0);
+
+    auto id = get_id(path);
+
+    mov(path, id, ".out", ".std.out");
+    mov(path, id, ".in", ".std.in");
+    mov(path, id, ".sy.ll", ".sy.ll");
+    printf("running %s.sy\n", path.c_str());
+    cmp(id);
+}
+
+//void run_staisp(String file) {
+//    int res =
+//        system((String("../staisp_frontend ../..") + file + ".sta").c_str());
+//    ASSERT_EQ(res, 0);
+//    system((String("cp ../../") + file + ".out 1.std.out").c_str());
+//    system((String("cp ../../") + file + ".in 1.std.in").c_str());
+//    system((String("cp ../../") + file + ".sta.opt.ll 1.sta.opt.ll").c_str());
+//    system((String("cp ../../") + file + ".sta.ll 1.sta.ll").c_str());
+//    printf("running %s.sta\n", file.c_str());
+//    cmp("1.sta.ll");
+//    cmp("1.sta.opt.ll");
+//}
