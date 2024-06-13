9fcc875eac0366b78988c2a5b794322e114acba6
XuKaFy <xukafy@163.com>
2024-06-13 23:10:42 +0800

    现在初始化函数也会被内联了

--- src/opt.cpp
+++ src/opt.cpp
@@ -19,11 +19,110 @@ int from_top_analysis(Ir::BlockedProgram &p);
 template <typename BlockValue, typename Utils>
 int from_bottom_analysis(Ir::BlockedProgram &p);
 
-void func_inline(Ir::pFuncDefined func, AstToIr::Convertor &convertor)
+void func_inline_from_bp(Ir::CallInstr* call_instr, Ir::BlockedProgram &new_p)
 {
-    // some function is generated manually, so don't need to be inlined
-    if(!func->ast_root)
-        return ;
+    /*
+    before:
+        OriginalBlock:
+            ...front...
+            [%n = ] call (ty) %func (args...)
+            ...back...
+    after:
+        FrontBlock:
+            ...
+            (if not void) %m = alloca (ty)
+            goto BodyBlock
+        (new) BodyBlock:
+             ...
+             all ret modified to:
+            * (if not void) store (ret_value), %m
+            * br BackBlock
+        (new) BackBlock:
+            (if not void) %n = load (ty) %m
+            ...
+    */
+    Ir::BlockedProgram* fun = call_instr->block->program;
+
+    // printf("***REPLACE %s\n", call_instr->instr_print().c_str());
+
+    Ir::Block* frontBlock = call_instr->block;
+    Ir::pBlock backBlock = fun->make_block();
+    backBlock->push_back(Ir::make_label_instr());
+
+    Ir::pInstr alloca_instr;
+    if (call_instr->ty->type_type() != TYPE_VOID_TYPE)
+        alloca_instr = Ir::make_alloc_instr(call_instr->ty);
+
+    // Step 1: copy the will-inline function and replace
+    //     all arguments with new arguments
+    for (size_t i = 1; i < call_instr->operand_size(); ++i) {
+        new_p.params[i-1]->replace_self(call_instr->operand(i)->usee);
+    }
+    // Step 2: split original block where CallInstr exists
+    auto call_instr_at = frontBlock->body.begin();
+    for (; call_instr_at != frontBlock->body.end(); ++call_instr_at) {
+        if (call_instr_at->get() == call_instr) {
+            break;
+        }
+    }
+    Ir::pInstr call_instr_saver = *call_instr_at;
+    for (auto i = std::next(call_instr_at); i != frontBlock->body.end(); ++i) {
+        backBlock->push_back(*i);
+        (*i)->block = backBlock.get();
+    }
+    frontBlock->body.erase(call_instr_at, frontBlock->body.end());
+    // Step 3: change call to br
+    if (alloca_instr)
+        fun->blocks.front()->push_behind_end(alloca_instr);
+    frontBlock->push_back(make_br_instr(new_p.blocks.front()->label()));
+    // Step 4: replace all ret in copied program to two statement:
+    // 1. (if not void) store my value to 
+    // 2. jump to BackBlock
+    for (auto i : new_p.blocks) {
+        if (i->back()->instr_type() == Ir::INSTR_RET) {
+            Ir::pInstr ret_instr = i->back();
+            i->body.pop_back();
+            if (alloca_instr) {
+                // printf("new alloca type = %s\n", alloca_instr->ty->type_name().c_str());
+                i->push_back(Ir::make_store_instr(alloca_instr.get(),
+                                  ret_instr->operand(0)->usee));
+                // printf("generated store: %s\n", i->body.back()->instr_print().c_str());
+            }
+            i->push_back(make_br_instr(backBlock->label()));
+        }
+    }
+    // Step 5: load ret value (if exists)
+    if (alloca_instr) {
+        auto load_instr = make_load_instr(alloca_instr.get());
+        backBlock->push_after_label(load_instr);
+        call_instr->replace_self(load_instr.get());
+        load_instr->block = backBlock.get();
+    }
+    // Step 5.99: move old alloca to first block
+    for (auto i = new_p.blocks.front()->body.begin(); i != new_p.blocks.front()->body.end(); ) {
+        if ((*i)->instr_type() == Ir::INSTR_ALLOCA) {
+            fun->blocks.front()->push_after_label(*i);
+            i = new_p.blocks.front()->body.erase(i);
+        } else ++i;
+    }
+    // Step 6: add new blocks to original function
+    for (auto i : new_p.blocks) {
+        fun->blocks.push_back(i);
+        i->program = fun;
+    }
+    fun->blocks.push_back(backBlock);
+    // Step 7: move imms to original function
+    for (auto i : new_p.imms) {
+        fun->add_imm(i);
+    }
+    // Step 8: clear new_p
+    new_p.blocks.clear();
+    new_p.imms.clear();
+    new_p.params.clear();
+}
+
+bool func_inline(Ir::pFuncDefined func, AstToIr::Convertor &convertor)
+{
     // printf("try to inline %s\n", func->name().c_str());
     Vector<Ir::CallInstr*> calls;
     for(const auto &use : func->users) {
@@ -35,114 +134,35 @@ void func_inline(Ir::pFuncDefined func, AstToIr::Convertor &convertor)
             // printf("user: %s\n", call_instr->instr_print().c_str());
         }
     }
+
+    if (calls.size() == 1 && !func->ast_root) { // __buildin_initializer has no ast_root
+        // printf("%s move codes\n", func->name().c_str());
+        func_inline_from_bp(calls.front(), func->p); // delete self
+        return true;
+    }
+
     for(auto call_instr : calls) {
-         /*
-        before:
-            OriginalBlock:
-                ...front...
-                [%n = ] call (ty) %func (args...)
-                ...back...
-        after:
-            FrontBlock:
-                ...
-                (if not void) %m = alloca (ty)
-                goto BodyBlock
-            (new) BodyBlock:
-                 ...
-                 all ret modified to:
-                * (if not void) store (ret_value), %m
-                * br BackBlock
-            (new) BackBlock:
-                (if not void) %n = load (ty) %m
-                ...
-        */
         Ir::BlockedProgram* fun = call_instr->block->program;
-
-        if(&func->p == fun) // don't inline self
+        if (&func->p == fun) // don't inline self
             continue;
-
-        // printf("***REPLACE %s\n", call_instr->instr_print().c_str());
-
-        Ir::Block* frontBlock = call_instr->block;
-        Ir::pBlock backBlock = fun->make_block();
-        backBlock->push_back(Ir::make_label_instr());
-
-        Ir::pInstr alloca_instr;
-        if (call_instr->ty->type_type() != TYPE_VOID_TYPE)
-            alloca_instr = Ir::make_alloc_instr(call_instr->ty);
-
-        // Step 1: copy the will-inline function and replace
-        //     all arguments with new arguments
+        
         Ir::pFuncDefined new_fun = convertor.generate_inline_function(func->ast_root);
         Ir::BlockedProgram new_p = new_fun->p;
-        for (size_t i = 1; i < call_instr->operand_size(); ++i) {
-            new_p.params[i-1]->replace_self(call_instr->operand(i)->usee);
-        }
-        // Step 2: split original block where CallInstr exists
-        auto call_instr_at = frontBlock->body.begin();
-        for (; call_instr_at != frontBlock->body.end(); ++call_instr_at) {
-            if (call_instr_at->get() == call_instr) {
-                break;
-            }
-        }
-        Ir::pInstr call_instr_saver = *call_instr_at;
-        for (auto i = std::next(call_instr_at); i != frontBlock->body.end(); ++i) {
-            backBlock->push_back(*i);
-            (*i)->block = backBlock.get();
-        }
-        frontBlock->body.erase(call_instr_at, frontBlock->body.end());
-        // Step 3: change call to br
-        if (alloca_instr)
-            fun->blocks.front()->push_behind_end(alloca_instr);
-        frontBlock->push_back(make_br_instr(new_p.blocks.front()->label()));
-        // Step 4: replace all ret in copied program to two statement:
-        // 1. (if not void) store my value to 
-        // 2. jump to BackBlock
-        for (auto i : new_p.blocks) {
-            if (i->back()->instr_type() == Ir::INSTR_RET) {
-                Ir::pInstr ret_instr = i->back();
-                i->body.pop_back();
-                if (alloca_instr) {
-                    // printf("new alloca type = %s\n", alloca_instr->ty->type_name().c_str());
-                    i->push_back(Ir::make_store_instr(alloca_instr.get(),
-                                      ret_instr->operand(0)->usee));
-                    // printf("generated store: %s\n", i->body.back()->instr_print().c_str());
-                }
-                i->push_back(make_br_instr(backBlock->label()));
-            }
-        }
-        // Step 5: load ret value (if exists)
-        if (alloca_instr) {
-            auto load_instr = make_load_instr(alloca_instr.get());
-            backBlock->push_after_label(load_instr);
-            call_instr->replace_self(load_instr.get());
-            load_instr->block = backBlock.get();
-        }
-        // Step 5.99: move old alloca to first block
-        for (auto i = new_p.blocks.front()->body.begin(); i != new_p.blocks.front()->body.end(); ) {
-            if ((*i)->instr_type() == Ir::INSTR_ALLOCA) {
-                fun->blocks.front()->push_after_label(*i);
-                i = new_p.blocks.front()->body.erase(i);
-            } else ++i;
-        }
-        // Step 6: add new blocks to original function
-        for (auto i : new_p.blocks) {
-            fun->blocks.push_back(i);
-            i->program = fun;
-        }
-        fun->blocks.push_back(backBlock);
-        // Step 7: move imms to original function
-        for (auto i : new_p.imms) {
-            fun->add_imm(i);
-        }
+
+        func_inline_from_bp(call_instr, new_p);
     }
+    return false;
 }
 
 void inline_all_function(const Ir::pModule &mod, AstToIr::Convertor &convertor)
 {
+    // remove functions that be all inlined
+    mod->remove_unused_function();
     // inline all function that can be inlined
-    for (auto &&i : mod->funsDefined) {
-        func_inline(i, convertor);
+    for (auto i = mod->funsDefined.begin(); i != mod->funsDefined.end();) {
+        if (func_inline(*i, convertor)) {
+            i = mod->funsDefined.erase(i);
+        } else ++i;
     }
     // remove functions that be all inlined
     mod->remove_unused_function();
