9ffe1e0f203716f3572022b1eec3bbd812e78653
Yaossg <yaossg@outlook.com>
2024-06-20 20:26:50 +0800

    std::list invalidates no iterator

--- src/reg2mem.cpp
+++ src/reg2mem.cpp
@@ -5,27 +5,19 @@
 
 void reg2mem(Ir::BlockedProgram& program) {
     for (auto&& block : program) {
-        std::vector<Ir::pInstr> self;
         for (auto it = block->begin(); it != block->end(); ++it) {
             if (auto phi = dynamic_cast<Ir::PhiInstr*>(it->get())) {
                 auto alloc = Ir::make_alloc_instr(phi->ty);
                 program.front()->push_after_label(alloc);
                 for (auto [label, val] : *phi) {
                     auto store = Ir::make_store_instr(alloc.get(), val);
-                    if (label->block() == block.get()) {
-                        self.push_back(store);
-                    } else {
-                        label->block()->push_behind_end(store);
-                    }
+                    label->block()->push_behind_end(store);
                 }
                 auto load = Ir::make_load_instr(alloc);
                 phi->replace_self(load.get());
                 it = block->erase(it);
                 it = block->insert(it, load);
             }
-        }
-        for (auto&& store : self) {
-            block->push_behind_end(store);
         }
     }
 }
