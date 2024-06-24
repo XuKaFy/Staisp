fb47e03ba73810ef3a408cc9f149a8e79d3ce79b
kotoriaster <xyg_aster@outlook.com>
2024-06-24 20:59:43 +0800

    computes backedge

--- include/alys_loop.h
+++ include/alys_loop.h
@@ -3,18 +3,23 @@
 #pragma once
 #include "alys_dom.h"
 #include "ir_block.h"
-#include <sys/stat.h>
-
 namespace Alys {
 
 struct NaturalLoop {
     Ir::Block *header;
     Ir::Block *latch;
     Set<Ir::Block *> loop_blocks;
+    NaturalLoop() = delete;
+    NaturalLoop(Ir::Block *header, Ir::Block *latch)
+        : header(header), latch(latch) {
+        complete_loop();
+    }
+    void complete_loop();
 };
 
 using pNaturalLoop = std::shared_ptr<NaturalLoop>;
-[[nodiscard]] pNaturalLoop make_natural_loop();
+[[nodiscard]] pNaturalLoop make_natural_loop(Ir::Block *header,
+                                             Ir::Block *latch);
 
 struct LoopInfo {
     Vector<pNaturalLoop> loops;
@@ -25,6 +30,8 @@ struct LoopInfo {
     static inline bool
     is_dom(Ir::Block *a, Ir::Block *b,
            const Map<Ir::Block *, Set<Ir::Block *>> &dom_set);
+    // dom context for dominator relation
+    auto build_dom_set(Ir::Block *bb, Set<Ir::Block *> idom_dom_set) -> void;
     void print_loop() const;
 };
 } // namespace Alys
--- src/alys_loop.cc
+++ src/alys_loop.cc
@@ -1,65 +1,63 @@
 
 #include "alys_loop.h"
+#include "def.h"
+#include "ir_block.h"
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <utility>
 namespace Alys {
 
-[[nodiscard]] pNaturalLoop make_natural_loop() {
-    return std::make_shared<NaturalLoop>();
+[[nodiscard]] pNaturalLoop make_natural_loop(Ir::Block *header,
+                                             Ir::Block *latch) {
+    auto ret_loop = std::make_shared<NaturalLoop>(header, latch);
+    return std::move(ret_loop);
+}
+
+void NaturalLoop::complete_loop() {
+    auto &loop_nodes = loop_blocks;
+    Vector<Ir::Block *> blk_stk;
+    blk_stk.push_back(latch);
+    while (!blk_stk.empty()) {
+        auto blk = blk_stk.back();
+        blk_stk.pop_back();
+        if (loop_nodes.insert(blk).second) {
+            for (auto pred : blk->in_blocks()) {
+                if (pred != header) {
+                    blk_stk.push_back(pred);
+                }
+            }
+        }
+    }
+    loop_nodes.insert(header);
 }
 
 LoopInfo::LoopInfo(Ir::BlockedProgram &p, const DomTree &dom_ctx) {
     my_assert(dom_ctx.unreachable_blocks.empty(), "no unreachable blocks");
     Map<Ir::Block *, Set<Ir::Block *>> dom_set;
-    std::function<void(Ir::Block *)> build_dom_set =
-        [&dom_ctx, &dom_set, &build_dom_set](Ir::Block *bb) -> void {
-        auto dom_bb = dom_ctx.dom_map.at(bb);
-        auto idom = dom_bb->idom;
-        auto idom_dom_set =
-            idom ? dom_set[idom->basic_block] : Set<Ir::Block *>{};
+    auto build_dom_set = [&dom_set,
+                          &dom_ctx](const auto &build_dom_set, Ir::Block *bb,
+                                    Set<Ir::Block *> idom_dom_set) -> void {
         idom_dom_set.insert(bb);
-        dom_set.insert({bb, idom_dom_set});
-        for (auto dom_succ : dom_bb->out_block) {
-            build_dom_set(dom_succ->basic_block);
-        }
+        dom_set[bb] = idom_dom_set;
+        printf("%s dom_set : %d\n", bb->name().c_str(), idom_dom_set.size());
+        for (auto succ : dom_ctx.dom_map.at(bb)->out_block)
+            build_dom_set(build_dom_set, succ->basic_block, idom_dom_set);
     };
 
-    auto build_loop_from_back_edges = [this](Ir::Block *latch,
-                                             Ir::Block *head) {
-        auto loop = make_natural_loop();
-        loop->latch = latch;
-        loop->header = head;
-        auto &loop_nodes = loop->loop_blocks;
-        loop_nodes.insert(latch);
-        Vector<Ir::Block *> blk_stk;
-        blk_stk.push_back(latch);
-        while (!blk_stk.empty()) {
-            auto blk = blk_stk.back();
-            blk_stk.pop_back();
-            loop->loop_blocks.insert(blk);
-            if (loop_nodes.insert(blk).second) {
-                for (auto pred : blk->in_blocks()) {
-                    if (pred != head) {
-                        blk_stk.push_back(pred);
-                    }
-                }
-            }
-        }
-        loop_nodes.insert(head);
-        loops.push_back(std::move(loop));
-    };
-    build_dom_set(p.front().get());
+    build_dom_set(build_dom_set, dom_ctx.order().front(), Set<Ir::Block *>{});
 
-    Vector<Pair<Ir::Block *, Ir::Block *>> back_edges;
     for (auto const &bb : dom_ctx.order()) {
-        auto br_instr = bb->back();
-        if (br_instr->instr_type() != Ir::INSTR_BR_COND)
-            continue;
-        auto br = dynamic_cast<Ir::BrCondInstr *>(br_instr.get());
-        auto true_label = dynamic_cast<Ir::LabelInstr *>(br->operand(1)->usee);
-        my_assert(true_label, "label");
-        if (!is_dom(bb, true_label->block(), dom_set))
-            continue;
-        back_edges.push_back({bb, true_label->block()});
-        build_loop_from_back_edges(bb, true_label->block());
+        Ir::Block *find = nullptr;
+        auto succ_bbs = bb->out_blocks();
+        auto &dom_bbs = dom_set.at(bb);
+        for (auto dom : dom_bbs) {
+            if (succ_bbs.count(dom) > 0)
+                // my_assert(!find, "no multiple head");
+                find = dom;
+            loops.push_back(make_natural_loop(dom, bb));
+        }
     }
 
     print_loop();
@@ -68,18 +66,23 @@ LoopInfo::LoopInfo(Ir::BlockedProgram &p, const DomTree &dom_ctx) {
 inline bool
 LoopInfo::is_dom(Ir::Block *a, Ir::Block *b,
                  const Map<Ir::Block *, Set<Ir::Block *>> &dom_set) {
-    return dom_set.at(a).count(b);
+    return dom_set.at(a).count(b) > 0;
 }
 
 void LoopInfo::print_loop() const {
+    std::string ret;
     for (auto loop : loops) {
-        printf("Loop header: %s, latch: %s\n", loop->header->name().c_str(),
-               loop->latch->name().c_str());
-        puts("\tloop blks: ");
+        ret += "loop header: ";
+        ret += loop->header->label()->name();
+        ret += "loop latch: ";
+        ret += loop->latch->label()->name();
+        ret += "\n\tloop blks: ";
         for (auto blk : loop->loop_blocks) {
-            printf("%s ", blk->name().c_str());
+            ret += blk->label()->name() += " ";
         }
-        puts("");
+        ret += "\n";
     }
+
+    std::cerr << ret;
 }
 } // namespace Alys
