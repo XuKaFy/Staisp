00a4b6133b553356846a698a7668232e4eced473
Yaossg <yaossg@outlook.com>
2024-07-01 23:08:33 +0800

    properly handle reg or imm

--- src/convert_ir_to_asm.cpp
+++ src/convert_ir_to_asm.cpp
@@ -74,272 +74,147 @@ Backend::Block Convertor::convert(const Ir::pFuncDefined &func, const Ir::pBlock
     return bkd_block;
 }
 
-template<typename T>
-T selector(ImmType ty,
-    T i32_use,
-    T i64_use,
-    T u32_use,
-    T u64_use)
-{
-    switch (ty) {
-    case IMM_I1:
-    case IMM_I8:
-    case IMM_I16:
-    case IMM_I32:
-        return i32_use;
-    case IMM_I64:
-        return i64_use;
-    case IMM_U1:
-    case IMM_U8:
-    case IMM_U16:
-    case IMM_U32:
-        return u32_use;
-    case IMM_U64:
-        return u64_use;
-    default:
-        break;
+struct ConvertBulk {
+    Backend::MachineInstrs bulk;
+    int allocate_register = 0;
+
+    void add(Backend::MachineInstr const& value) {
+        bulk.push_back(value);
     }
-    my_assert(false, "?");
-}
 
-template<typename T>
-T selector(ImmType ty,
-    T f32_use,
-    T f64_use)
-{
-    switch (ty) {
-    case IMM_F32:
-        return f32_use;
-    case IMM_F64:
-        return f64_use;
-    default: break;
+    template<typename T>
+    T selector(ImmType ty,
+        T i32_use,
+        T i64_use) {
+        switch (ty) {
+            case IMM_I1:
+            case IMM_I8:
+            case IMM_I16:
+            case IMM_I32:
+            case IMM_U1:
+            case IMM_U8:
+            case IMM_U16:
+            case IMM_U32:
+                return i32_use;
+            case IMM_I64:
+            case IMM_U64:
+                return i64_use;
+            default:
+                break;
+        }
+        my_assert(false, "?");
     }
-    my_assert(false, "?");
-}
 
-Backend::MachineInstr selector(
-    ImmType ty, int rd, int rs, int rt,
-    Backend::FRegFRegInstrType r_f32_use,
-    Backend::FRegFRegInstrType r_f64_use) {
-    auto i = Backend::FRegFRegInstr {
-        selector(ty, r_f32_use, r_f64_use),
-        (Backend::FReg) rd, (Backend::FReg) rs, (Backend::FReg) rt };
-    return Backend::MachineInstr {i};
-}
-
-Backend::MachineInstr selector(
-    ImmType ty, int rd, int rs, int rt,
-    Backend::RegRegInstrType r_i32_use,
-    Backend::RegRegInstrType r_i64_use,
-    Backend::RegRegInstrType r_u32_use,
-    Backend::RegRegInstrType r_u64_use) {
-    auto i = Backend::RegRegInstr {
-        selector(ty, r_i32_use, r_i64_use, r_u32_use, r_u64_use),
-        (Backend::Reg) rd, (Backend::Reg) rs, (Backend::Reg) rt };
-    return Backend::MachineInstr {i};
-}
-
-Backend::MachineInstr selector(
-    ImmType ty, int rd, int rs, int rt, int imm,
-    Backend::RegRegInstrType r_i32_use,
-    Backend::RegRegInstrType r_i64_use,
-    Backend::RegRegInstrType r_u32_use,
-    Backend::RegRegInstrType r_u64_use,
-    Backend::RegImmInstrType i_i32_use,
-    Backend::RegImmInstrType i_i64_use,
-    Backend::RegImmInstrType i_u32_use,
-    Backend::RegImmInstrType i_u64_use) {
-    if (rt != -1) {
-        auto i = Backend::RegRegInstr {
-            selector(ty, r_i32_use, r_i64_use, r_u32_use, r_u64_use),
-            (Backend::Reg) rd, (Backend::Reg) rs, (Backend::Reg) rt };
-        return Backend::MachineInstr {i};
+    Backend::Reg toReg(Ir::Val* val, Backend::Reg tmp) {
+        auto name = val->name();
+        // if it is a register, use it
+        if (name[0] == '%') {
+            try {
+                return (Backend::Reg) std::stoi(name.substr(1));
+            } catch (std::invalid_argument&) {
+                return (Backend::Reg) --allocate_register;
+            }
+        }
+        // if it is a immediate, load it
+        add({Backend::ImmInstr {
+            Backend::ImmInstrType::LI, tmp, std::stoi(name)}
+        });
+        return tmp;
     }
-    auto i = Backend::RegImmInstr {
-            selector(ty, i_i32_use, i_i64_use, i_u32_use, i_u64_use),
-            (Backend::Reg) rd, (Backend::Reg) rs, imm };
-    return Backend::MachineInstr {i};
-}
 
-int to_int(std::string name)
-{
-    int a = 0;
-    for (auto i = name.rbegin(); i != std::prev(name.rend()); ++i) {
-        a = a * 10 + *i;
-    };
-    return a;
-}
-
-int string_to_reg(std::string name)
-{
-    if (name[0] != '%') {
-        return -1; // imm
+    Backend::RegRegInstrType selectRRType(ImmType ty, Ir::BinInstrType bin) {
+        switch (bin) {
+            case Ir::INSTR_ADD:
+                return selector(ty,
+                    Backend::RegRegInstrType::ADD,
+                    Backend::RegRegInstrType::ADDW);
+            case Ir::INSTR_SUB:
+                return selector(ty,
+                    Backend::RegRegInstrType::SUB,
+                    Backend::RegRegInstrType::SUBW);
+            case Ir::INSTR_MUL:
+                return selector(ty,
+                    Backend::RegRegInstrType::MUL,
+                    Backend::RegRegInstrType::MULW);
+            case Ir::INSTR_SDIV:
+            case Ir::INSTR_UDIV:
+                return selector(ty,
+                    Backend::RegRegInstrType::DIV,
+                    Backend::RegRegInstrType::DIVW);
+            case Ir::INSTR_SREM:
+            case Ir::INSTR_UREM:
+                return selector(ty,
+                    Backend::RegRegInstrType::REM,
+                    Backend::RegRegInstrType::REMW);
+                break;
+            case Ir::INSTR_XOR:
+                std::abort();
+            case Ir::INSTR_AND:
+                return Backend::RegRegInstrType::AND;
+            case Ir::INSTR_OR:
+                return Backend::RegRegInstrType::OR;
+            case Ir::INSTR_ASHR:
+                return selector(ty,
+                    Backend::RegRegInstrType::SRA,
+                    Backend::RegRegInstrType::SRAW);
+                case Ir::INSTR_LSHR:
+                return selector(ty,
+                    Backend::RegRegInstrType::SRL,
+                    Backend::RegRegInstrType::SRLW);
+            case Ir::INSTR_SHL:
+                return selector(ty,
+                    Backend::RegRegInstrType::SLL,
+                    Backend::RegRegInstrType::SLLW);
+        }
+        std::abort();
     }
-    return to_int(name.substr(1));
-}
 
-Backend::MachineInstr convert_binary_instr(const Pointer<Ir::BinInstr> &instr)
-{
-    my_assert(instr, "?");
-    switch (instr->binType) {
-    case Ir::INSTR_ADD:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            to_int(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::ADD,
-            Backend::RegRegInstrType::ADDW,
-            Backend::RegRegInstrType::ADD,
-            Backend::RegRegInstrType::ADDW,
-            Backend::RegImmInstrType::ADDI,
-            Backend::RegImmInstrType::ADDIW,
-            Backend::RegImmInstrType::ADDI,
-            Backend::RegImmInstrType::ADDIW);
-    case Ir::INSTR_SUB:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            -to_int(instr->operand(1)->usee->name()), // imm is negative
-            Backend::RegRegInstrType::SUB,
-            Backend::RegRegInstrType::SUBW,
-            Backend::RegRegInstrType::SUB,
-            Backend::RegRegInstrType::SUBW,
-            Backend::RegImmInstrType::ADDI,
-            Backend::RegImmInstrType::ADDIW,
-            Backend::RegImmInstrType::ADDI,
-            Backend::RegImmInstrType::ADDIW);
-    case Ir::INSTR_MUL:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::MUL,
-            Backend::RegRegInstrType::MULW,
-            Backend::RegRegInstrType::MUL,
-            Backend::RegRegInstrType::MULW);
-    case Ir::INSTR_FADD:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::FRegFRegInstrType::FADD_S,
-            Backend::FRegFRegInstrType::FADD_S);
-    case Ir::INSTR_FSUB:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::FRegFRegInstrType::FSUB_S,
-            Backend::FRegFRegInstrType::FSUB_S);
-    case Ir::INSTR_FMUL:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::FRegFRegInstrType::FMUL_S,
-            Backend::FRegFRegInstrType::FMUL_S);
-    case Ir::INSTR_SDIV:
-    case Ir::INSTR_UDIV:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::DIV,
-            Backend::RegRegInstrType::DIVW,
-            Backend::RegRegInstrType::DIV,
-            Backend::RegRegInstrType::DIVW);
-    case Ir::INSTR_SREM:
-    case Ir::INSTR_UREM:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::REM,
-            Backend::RegRegInstrType::REMW,
-            Backend::RegRegInstrType::REM,
-            Backend::RegRegInstrType::REMW);
-        break;
-    case Ir::INSTR_FDIV:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::FRegFRegInstrType::FDIV_S,
-            Backend::FRegFRegInstrType::FDIV_S);
-    case Ir::INSTR_FREM:
-        break;
-    case Ir::INSTR_XOR:
-    case Ir::INSTR_AND:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::AND,
-            Backend::RegRegInstrType::AND,
-            Backend::RegRegInstrType::AND,
-            Backend::RegRegInstrType::AND);
-    case Ir::INSTR_OR:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::OR,
-            Backend::RegRegInstrType::OR,
-            Backend::RegRegInstrType::OR,
-            Backend::RegRegInstrType::OR);
-    case Ir::INSTR_ASHR:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::SRA,
-            Backend::RegRegInstrType::SRAW,
-            Backend::RegRegInstrType::SRA,
-            Backend::RegRegInstrType::SRAW);
-        case Ir::INSTR_LSHR:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::SRL,
-            Backend::RegRegInstrType::SRLW,
-            Backend::RegRegInstrType::SRL,
-            Backend::RegRegInstrType::SRLW);
-    case Ir::INSTR_SHL:
-        return selector(to_basic_type(instr->ty)->ty,
-            string_to_reg(instr->name()),
-            string_to_reg(instr->operand(0)->usee->name()),
-            string_to_reg(instr->operand(1)->usee->name()),
-            Backend::RegRegInstrType::SLL,
-            Backend::RegRegInstrType::SLLW,
-            Backend::RegRegInstrType::SLL,
-            Backend::RegRegInstrType::SLLW);
-        break;
+
+    // note: float is not considered yet
+
+
+
+    void convert_binary_instr(const Pointer<Ir::BinInstr> &instr) {
+        auto rd = toReg(instr.get(), {});
+        auto rs1 = toReg(instr->operand(0)->usee, rd);
+        auto rs2 = toReg(instr->operand(1)->usee, rd);
+        add({ Backend::RegRegInstr {
+            selectRRType(to_basic_type(instr->ty)->ty, instr->binType), rd, rs1, rs2,
+        } });
     }
-    std::abort(); // no test case should reach here
-}
+
+    void convert_return_instr(const Pointer<Ir::RetInstr> &instr) {
+        auto ret = std::static_pointer_cast<Ir::RetInstr>(instr);
+        if (ret->operand_size() > 0) {
+            auto a0 = Backend::Reg::A0;  // a0 is for return value
+            add({ Backend::RegInstr{
+                Backend::RegInstrType::MV,
+                a0,
+                toReg(ret->operand(0)->usee, a0)
+            } });
+        }
+        add({ Backend::ReturnInstr{} });
+    }
+
+};
 
 Backend::MachineInstrs Convertor::convert(const Ir::pFuncDefined &func, const Ir::pInstr &instr)
 {
-    Backend::MachineInstrs res;
+    ConvertBulk bulk;
     switch (instr->instr_type()) {
     case Ir::INSTR_RET:
-        // return value is not yet proceeded
-        res.push_back({ Backend::ReturnInstr{} });
+        bulk.convert_return_instr(std::dynamic_pointer_cast<Ir::RetInstr>(instr));
         break;
     case Ir::INSTR_BR:
-        // label is not yet proceeded
-        res.push_back({ Backend::JInstr{ func->name() + "_" + instr->operand(0)->usee->name() } });
+        bulk.add({ Backend::JInstr{ func->name() + "_" + instr->operand(0)->usee->name() } });
         break;
     case Ir::INSTR_BR_COND:
         // condition is not yet proceeded
-        res.push_back({ Backend::BranchInstr{Backend::BranchInstrType::BEQ, {}, {}, "TODO" } });
+        bulk.add({ Backend::BranchInstr{Backend::BranchInstrType::BEQ, {}, {}, "TODO" } });
         break;
     case Ir::INSTR_CALL:
         // arguments are not yet proceeded
-        res.push_back({ Backend::CallInstr{instr->operand(0)->usee->name()} });
+        bulk.add({ Backend::CallInstr{instr->operand(0)->usee->name()} });
         break;
     case Ir::INSTR_CAST:
     case Ir::INSTR_CMP:
@@ -348,7 +223,7 @@ Backend::MachineInstrs Convertor::convert(const Ir::pFuncDefined &func, const Ir
     case Ir::INSTR_UNARY:
         break;
     case Ir::INSTR_BINARY:
-        res.push_back(convert_binary_instr(std::static_pointer_cast<Ir::BinInstr>(instr)));
+        bulk.convert_binary_instr(std::static_pointer_cast<Ir::BinInstr>(instr));
         break;
     case Ir::INSTR_ITEM:
         break;
@@ -361,7 +236,7 @@ Backend::MachineInstrs Convertor::convert(const Ir::pFuncDefined &func, const Ir
         // just ignore all these ir
         break;
     }
-    return res;
+    return bulk.bulk;
 }
 
 } // namespace BackendConvertor
