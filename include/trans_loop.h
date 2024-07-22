65b6a4a19160b908a7208f6dbefba2e5f9ac2e0f
KotoriAster <71951124+KotoriAster@users.noreply.github.com>
2024-07-22 18:03:37 +0800

    IndVarPruning add some predicates

--- include/trans_loop.h
+++ include/trans_loop.h
@@ -2,7 +2,12 @@
 #pragma once
 
 #include "alys_dom.h"
+#include "alys_loop.h"
+#include "def.h"
 #include "ir_block.h"
+#include "ir_constant.h"
+#include "ir_instr.h"
+#include "ir_val.h"
 namespace Optimize {
 class Canonicalizer_pass {
     Ir::BlockedProgram &cur_func;
@@ -14,4 +19,37 @@ public:
     void ap();
     void rewrite_induction();
 };
+
+class IndVarPruning_pass {
+    Ir::BlockedProgram &cur_func;
+    Alys::DomTree &dom_ctx;
+    Map<Ir::Block *, Set<Ir::Block *>> dom_set;
+
+public:
+    IndVarPruning_pass(Ir::BlockedProgram &arg_func, Alys::DomTree &arg_dom,
+                       Map<Ir::Block *, Set<Ir::Block *>> arg_dom_set = {})
+        : cur_func(arg_func), dom_ctx(arg_dom) {
+        if (arg_dom_set.empty())
+            dom_set = Alys::build_dom_set(dom_ctx);
+        dom_set = arg_dom_set;
+    }
+
+    void ap();
+
+    bool is_invariant(Ir::Val *val, Ir::Block *loop_hdr) {
+        if (val->type() == Ir::VAL_CONST)
+            return true;
+
+        my_assert(val->type() == Ir::VAL_INSTR, "instruction as value");
+        auto val_as_instr = dynamic_cast<Ir::Instr *>(val);
+        return val_as_instr->block() != loop_hdr;
+    }
+
+    bool is_Mono(Ir::Val *val, Ir::Block *loop_hdr, bool dir, int depth);
+};
+
+template <typename Tl, typename Tr>
+bool addtion_verifier(Ir::Val *val, Tl &lhs, Tr &rhs,
+                      const std::function<bool(Ir::Val *, Tl &)> &lf,
+                      const std::function<bool(Ir::Val *, Tr &)> &rf);
 } // namespace Optimize
--- src/trans_loop.cc
+++ src/trans_loop.cc
@@ -2,9 +2,17 @@
 #include "trans_loop.h"
 #include "alys_loop.h"
 #include "def.h"
+#include "ir_block.h"
 #include "ir_cmp_instr.h"
+#include "ir_constant.h"
 #include "ir_control_instr.h"
 #include "ir_instr.h"
+#include "ir_opr_instr.h"
+#include "ir_phi_instr.h"
+#include "ir_val.h"
+#include "type.h"
+#include <cstdint>
+#include <functional>
 
 namespace Optimize {
 
@@ -158,7 +166,7 @@ void Canonicalizer_pass::ap() {
             Alys::is_dom(r_label->block(), cur_bb.get(), dom_set) &&
             !Alys::is_dom(l_label->block(), cur_bb.get(), dom_set)) {
             back_instr->release_operand(1);
-            back_instr->release_operand(2);
+            back_instr->release_operand(1);
             back_instr->add_operand(r_label);
             back_instr->add_operand(l_label);
             negate_cmp(loop_comparator->cmp_type);
@@ -166,4 +174,92 @@ void Canonicalizer_pass::ap() {
         }
     }
 }
+
+bool IndVarPruning_pass::is_Mono(Ir::Val *val, Ir::Block *loop_hdr, bool dir,
+                                 int depth) {
+
+    auto val_as_instr = dynamic_cast<Ir::Instr *>(val);
+
+    std::function<bool(Ir::Val *, Ir::Val *&)> add_tautof =
+        [](Ir::Val *op_val, Ir::Val *&left_exactor) -> bool {
+        left_exactor = op_val;
+        return true;
+    };
+
+    std::function<bool(Ir::Val *, Ir::Const *&)> add_signed_constf =
+        [](Ir::Val *op_val, Ir::Const *&right_exactor) -> bool {
+        if (is_signed_type(op_val->ty) && op_val->type() == Ir::VAL_CONST) {
+            right_exactor = dynamic_cast<Ir::Const *>(op_val);
+            return true;
+        }
+        return false;
+    };
+
+    std::function<bool(Ir::Val *, const Ir::Val *&)> add_idf =
+        [](Ir::Val *op_val, const Ir::Val *&arg_val) -> bool {
+        return arg_val == op_val;
+    };
+    if (val_as_instr->instr_type() == Ir::INSTR_PHI) {
+        auto phi_instr = dynamic_cast<Ir::PhiInstr *>(val_as_instr);
+        constexpr intmax_t maxIncrement = 1;
+        for (auto [blk_label, phi_usee] : *phi_instr) {
+            if (is_invariant(phi_usee, loop_hdr))
+                continue;
+
+            Ir::Const *step;
+            if (addtion_verifier(phi_usee, val, step, add_idf,
+                                 add_signed_constf))
+                return false;
+            if (dir) {
+                if (step->v.imm_value() <= 0)
+                    return false;
+                if (step <= maxIncrement)
+                    continue;
+            } else {
+                if (step >= 0)
+                    return false;
+                if (step >= -maxIncrement)
+                    continue;
+            }
+            return false;
+        }
+
+        return true;
+    }
+    if (depth < 0) {
+        return false;
+    }
+    Ir::Val *acc;
+    Ir::Const *inc;
+
+    if (addtion_verifier(val, acc, inc, add_tautof, add_signed_constf)) {
+        return is_Mono(val, loop_hdr, dir, depth - 1);
+    }
+    return false;
+}
+
+template <typename Tl, typename Tr>
+bool addtion_verifier(Ir::Val *val, Tl &lhs, Tr &rhs,
+                      const std::function<bool(Ir::Val *, Tl &)> &lf,
+                      const std::function<bool(Ir::Val *, Tr &)> &rf) {
+    auto val_as_instr = dynamic_cast<Ir::Instr *>(val);
+
+    if (val_as_instr->instr_type() != Ir::INSTR_BINARY)
+        return false;
+    auto bin_instr = dynamic_cast<Ir::BinInstr *>(val_as_instr);
+    if (bin_instr->binType != Ir::INSTR_ADD)
+        return false;
+    auto fst_op = bin_instr->operand(0)->usee;
+    auto snd_op = bin_instr->operand(1)->usee;
+
+    if (lf(fst_op, lhs) && rf(snd_op, rhs)) {
+        return true;
+    }
+    if (lf(snd_op, lhs) && rf(fst_op, rhs)) {
+        return true;
+    }
+
+    return false;
+}
+
 } // namespace Optimize
